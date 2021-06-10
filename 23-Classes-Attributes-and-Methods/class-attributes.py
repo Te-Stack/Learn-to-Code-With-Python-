e50d07ba964260698a91f74aacdbeac7f72e3290
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-06-10 22:39:54 +0100

    Data Structures

--- 23-Classes-Attributes-and-Methods/class-attributes.py
+++ 23-Classes-Attributes-and-Methods/class-attributes.py
@@ -25,22 +25,26 @@ print(c2.count)
 print(c3.count)
 
 
-class Employee(object):
+class Employee():
     numEmployee = 0
 
     def __init__(self, name, rate):
-    self.owed = 0
-    self.name = name
-    self.rate=rate
-    Employee.numEmployee += 1
+        self.owed = 0
+        self.name = name
+        self.rate=rate
+        Employee.numEmployee += 1
 
     def __del__(self):
-    Employee.numEmployee -= 1
+        Employee.numEmployee -= 1
 
     def hours(self, numHours):
-    self.owed += numHours * self.rate
-    return("%.2f hours worked" % numHours)
+        self.owed += numHours * self.rate
+        return(f"{numHours} hours worked")
 
     def pay(self):
-    self.owed = 0
-    return("payed %s " % self.name)
+        self.owed = 0
+        return(f"payed {self.name}")
+
+
+emp1 = Employee("Quincy",15.2)
+print(emp1.hours(20))
