c9ed4205232745a760e02a2166b99b9dad4b6ad1
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-06-13 22:25:29 +0100

    Algorithm Task

--- 22-Classes-the-Basics/the__init__method.py
+++ 22-Classes-the-Basics/the__init__method.py
@@ -1,9 +1,11 @@
 class Guitar():
     def __init__(self):
         print(f"A new guitar is being created! This object is {self}")
+        print(f"A new guitar is being created! This object is {self}")
 
 acoustic = Guitar()    
 print  (acoustic)
+
 
 
 electric = Guitar()
