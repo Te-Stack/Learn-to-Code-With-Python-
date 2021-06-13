946a6cc38c5e5ff2b59c95919b5a399b8c939610
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-06-13 18:53:17 +0100

    Algorithm Task

--- 08-Control-Flow/Task.py
+++ 08-Control-Flow/Task.py
@@ -1,3 +1,9 @@
-# Return a set of identical items from set 1and set 2
+# 5. Return a set of identical items from set 1and set 2
 # set1={10,20,30,40,50}
 # set2={30,40,50,60,70}
+
+
+#6. Check if these two sets have any elements in common , if yes, display the common element 
+set1 = {10,20,30,40,50}
+
+set2={60,70,80,90,10}
