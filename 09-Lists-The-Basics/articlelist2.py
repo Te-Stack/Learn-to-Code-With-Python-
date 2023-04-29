3040fd286d293782f4e8a58aeffaae633f105490
Te-Stack <ukumakubequincy@gmail.com>
2023-04-29 22:49:18 +0100

    Article

--- 09-Lists-The-Basics/articlelist2.py
+++ 09-Lists-The-Basics/articlelist2.py
@@ -8,3 +8,12 @@ Quarterly_revenue = [15000,12511,12446,245424,286,248]
 print(length_hint(Quarterly_revenue))
 #Output: 6
 
+
+#Initial creation of the list containing different integers
+List_of_things = [1500, "sodas", True, 1.5]
+#Storing the result in a variabe 
+length_list = len(Quarterly_revenue)
+#Printing out the variable
+print(length_list)
+#After running the code the result:
+#4
--- 09-Lists-The-Basics/articleperformance3.py
+++ 09-Lists-The-Basics/articleperformance3.py
@@ -1,30 +1,62 @@
-# here, we will analyse the performance of our code
-# to find the length of the list
+# Analysing the performance of each methods in executing 
 
-# we are importing the length_hint function
-import timeit
+#Importing time, length_hint and numpy module
 import time
 from operator import length_hint
+import numpy as np
 
-# now, we will initialise our list with items in it
+#Creating our list containing integers 
+Quarterly_revenue = [15000,12511,12446,245424,286,248]
 
-list_of_items = [1, 3, 5, 7, 9, 11]
 
-# first, we will print the list of items as it is
+#Printing out the items in the list 
+print('The items in the list are: ' + str(Quarterly_revenue))
 
-print('The items in the list are: ' + str(list_of_items))
+# 1. Finding the length_list using the Naive method(For loop)
 
-# now, we will find the length of the list
-# we will initialise the length as 0
 
+# Taking note of the start time 
 begin_time_constraint = time.perf_counter()
-print(begin_time_constraint)
 
-
+# we will initialise the length as 0 and then use the for loop to get the list length
 length = 0
-for I in list_of_items:
+for I in Quarterly_revenue:
 
    length = length + 1
 
+#Taking note of the end time to know the time it took to execute
 end_time_constraint = str(time.perf_counter() - begin_time_constraint)
-print(end_time_constraint)
+
+# 2. Finding the length_list using the Built-in function len()
+
+# Taking note of the start time 
+begin_time_len = time.perf_counter()
+#use the built in len() to get the list length
+length_of_list = len(Quarterly_revenue)
+#Taking note of the end time to know the time it took to execute
+end_time_len = str(time.perf_counter() - begin_time_len)
+
+# 3. Finding the length_list using the length_hint() method
+
+# Taking note of the start time 
+begin_time_hint = time.perf_counter()
+#use the length_hint() method to get the list length
+length_hint_list = length_hint(Quarterly_revenue)
+#Taking note of the end time to know the time it took to execute
+end_time_hint = str(time.perf_counter() - begin_time_hint)
+
+
+#4. Finding the length_list using the numpy Module 
+
+# Taking note of the start time 
+begin_time_numpy = time.perf_counter()
+#use the numpy module to get the list length
+length_numpy_list = np.size(Quarterly_revenue)
+#Taking note of the end time to know the time it took to execute
+end_time_numpy = str(time.perf_counter() - begin_time_numpy)
+
+#printing out all the results
+print('The time taken by naive method to calculate the length of the list is: ' + end_time_constraint)
+print('The time taken by len method to calculate the length of the list is: ' + end_time_len)
+print('The time taken by length_hint method to calculate the length of the list is: ' + end_time_hint)
+print('The time taken by numpy module method to calculate the length of the list is: ' + end_time_numpy)
